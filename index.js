3979655b7c71643913e54789a24274b051e86808
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2023-05-30 17:11:58 +0530

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const getNumber = () => {
   let number = document.querySelectorAll(".drum").length;
-  console.log(number);
+  console.log("getnumber function ke andar chal rahe hain " + number);
   return number;
 };
 
@@ -52,3 +52,48 @@ function soundfunc(event) {
 }
 
 attachEvent();
+
+function soundfunc2(key) {
+  console.log("Soundfunc 2 is called");
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    default:
+      console.log("Wrong Input");
+      break;
+  }
+}
+
+document.addEventListener("keypress", function (event) {
+  let keyy = event.keyCode;
+  alert(event.key);
+  keyy = event.key;
+  console.log(keyy);
+  soundfunc2(keyy);
+});
